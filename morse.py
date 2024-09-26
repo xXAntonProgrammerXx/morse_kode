332aa60e2a628520a94d98e181e89ecb396a3662
Ranch3 <Noahsofus@gmail.com>
2024-09-26 14:15:25 +0200

    tilføjdede fra f til 0 i morseCode Dict
    Lavet translate til morese funktion

--- morse.py
+++ morse.py
@@ -1,12 +1,15 @@
 # Dictionary til oversættelse fra bogstaver til morsekode
-morseCode = {"a":".-", "b":"-...", "c":"-.-.", "d":"-..", "e":".", "f":"..-."}
+from idlelib.editor import keynames
+
+morseCode = {"a":".-", "b":"-...", "c":"-.-.", "d":"-..", "e":".", "f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--..","1":".----","2":"..---","3":"...--","4":"....-","5":".....","6":"-....","7":"--...","8":"---..","9":"----.","0":"-----"}
 
 # Dictionary til oversættelse fra morsekode til bogstaver. Tomt oversættes til mellemrum.
 morseCodeReverse = {}
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
-    pass
+    if letter in morseCode:
+        return morseCode[letter]
 
 # Denne funktion oversætter en vilkårlig tekststreng til morsekode
 # '/' markerer nyt bogstav
