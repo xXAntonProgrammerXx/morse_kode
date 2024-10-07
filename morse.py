3e08bf298c188d6db88a694caae86ee4452338cf
Ranch3 <Noahsofus@gmail.com>
2024-10-07 10:52:49 +0200

    -fik store bogstaver til at virke :D
    -fik program til at retunere "?" når den møder ukendt tegn

--- morse.py
+++ morse.py
@@ -1,5 +1,5 @@
 # Dictionary til oversættelse fra bogstaver til morsekode
-from idlelib.editor import keynames
+#from idlelib.editor import keynames
 
 morseCode = {"a":".-", "b":"-...", "c":"-.-.", "d":"-..", "e":".", "f":"..-.","g":"--.","h":"....","i":"..","j":".---","k":"-.-","l":".-..","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-","u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--..","1":".----","2":"..---","3":"...--","4":"....-","5":".....","6":"-....","7":"--...","8":"---..","9":"----.","0":"-----"}
 
@@ -8,9 +8,10 @@ morseCodeReverse = {}
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
+    letter=letter.lower()
     if letter in morseCode:
         return morseCode[letter]
-
+    else: return "?"
 # Denne funktion oversætter en vilkårlig tekststreng til morsekode
 # '/' markerer nyt bogstav
 # '//' markerer nyt ord
@@ -22,4 +23,4 @@ def encodeMessage(message, code):
 # '//' markerer nyt ord
 def decodeMessage(message, code):
     pass
-print (translate("a",morseCode))
+print (translate("A",morseCode))
